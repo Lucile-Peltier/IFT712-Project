abc49b895efc5a5cb3bc57d155360a1c29daee2f
Sergio Redondo <reds2401@users.noreply.github.com>
2020-11-05 16:10:48 -0500

    Update gestion_donnees.py

--- gestion_donnees.py
+++ gestion_donnees.py
@@ -11,8 +11,8 @@ import pandas as pd  # Analyse de données
 import os            # Se communiquer avec le système opérative  
 # import matplotlib.pyplot as plt
 import cv2           # Gérer les images
-# from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 # Définir les marqueurs du classe et les dossiers avec les images
 f_types = os.listdir(os.getcwd() + '/flowers')
@@ -38,10 +38,24 @@ class GestionDonnees :
                 img_n = cv2.resize(img, (150, 150))
                 x_base.append(np.array(img_n))
                 t_base.append(str(f_type))
-            pass
         return x_base, t_base
                 
+    def split_donnees(x_data, t_data, methode) :
+        
+        if methode==0 :
+            kf = KFold(10, True)
+            #for idx_train, idx_test in
+            kf_splits = kf.split(x_data)
+            #x_train = x_data[idx_train]
+            #t_train = t_data[idx_train]
+            #x_test = x_data[idx_test]
+            #t_test = t_data[idx_test]
+            return kf_splits
+        
+        else :
+            return 0
+            
 [xx, tt] = GestionDonnees.lecture_donnees(f_types, f_directories)
+kf_spl = GestionDonnees.split_donnees(xx,tt,0)
 
-print(xx, '\n')
-print(tt)
+print('Finished')
